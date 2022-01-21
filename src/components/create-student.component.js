121e72293568330a531aa861afbcc3be91d1d509
Samantha Malone <sammac99@gmail.com>
2022-01-20 18:11:18 -0700

    added bootstrap to create student component

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class CreateStudent extends Component {
   render() {
     return (
-      <div>
-        <p>React Create Student Component!</p>
+      <div class="form-wrapper">
+        <Form>
+          <Form.Group controlled="Name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+
+          <Form.Group controlled="Email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+
+          <Form.Group controlled="Name">
+            <Form.Label>Roll No</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+
+          <Button variant="danger" size="lg" block="block" type="submit">
+            Create Student
+          </Button>
+        </Form>
       </div>
     );
   }

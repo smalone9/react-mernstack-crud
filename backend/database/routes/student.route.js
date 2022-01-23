9b7d145625a884558622e0baaf7dbc1d4c53b4f8
Samantha Malone <sammac99@gmail.com>
2022-01-22 18:18:23 -0700

    created routes

---
+++ backend/database/routes/student.route.js
@@ -0,0 +1,74 @@
+let mongoose = require("mongoose"),
+  express = require("express"),
+  router = require("router");
+
+//Student Model
+let studentSchema = require("../Models/Student");
+
+//CREATE student
+router.route("/create-student").post((req, res, next) => {
+  studentSchema.create(req.body, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      console.log(data);
+      res.json(data);
+    }
+  });
+});
+
+//READ students
+router.route("/").get((req, res) => {
+  studentSchema.find((error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+//GET single student
+router.route("/edit-student/:id").get((req, res) => {
+  studentSchema.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+//UPDATE student
+router.route("/update-student/:id").put((req, res, next) => {
+  studentSchema.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error);
+      } else {
+        res.json(data);
+        console.log("Student updated successfully!");
+      }
+    }
+  );
+});
+
+//DELETE student
+router.route("/delete-student/:id").delete((req, res, next) => {
+  studentSchema.findByIdAndRemove(req.params.id, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
+
+module.exports = router;

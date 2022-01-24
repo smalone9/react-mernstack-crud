dfca18cf38f760e62eb7734c357a358d3d032fb0
Samantha Malone <sammac99@gmail.com>
2022-01-24 10:20:08 -0700

    changing https to http

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -42,7 +42,7 @@ export default class CreateStudent extends Component {
       rollno: this.state.rollno,
     };
     axios
-      .post("https://localhost:4000/students/create-student", studentObject)
+      .post("http://localhost:4000/students/create-student", studentObject)
       .then((res) => console.log(res.data));
 
     this.setState({ name: "", email: "", rollno: "" });

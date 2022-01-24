d6df07ef24cdfb606eeaf6cb12ad6458ebfcb7ad
Samantha Malone <sammac99@gmail.com>
2022-01-23 21:48:11 -0700

    connected database

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // db: "mongodb://sammac99:Blue999!@ElasticIP:27017//mongodb+srv://cluster0.of5c7.mongodb.net/React",
-  db: "mongodb://localhost:27017/reactdb",
+  // db: "mongodb://sammac99:Blue999!@localhost:27017//mongodb+srv://cluster0.of5c7.mongodb.net/React",
+  db: "mongodb://0.0.0.0:27017/reactdb",
 };

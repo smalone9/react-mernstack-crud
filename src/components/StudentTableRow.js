d34de112071711c93a4b815f669df8b2551622b0
Samantha Malone <sammac99@gmail.com>
2022-01-24 20:33:39 -0700

    built delete functionality

--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 export default class StudentTableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.deleteStudent = this.deleteStudent.bind(this);
+  }
+
+  deleteStudent() {
+    axios
+      .delete(
+        "http://localhost:4000/students/delete-student/" + this.props.obj._id
+      )
+      .then((res) => {
+        console.log("Student deleted successfully!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <tr>
@@ -16,7 +35,7 @@ export default class StudentTableRow extends Component {
           >
             Edit
           </Link>
-          <Button size="sm" variant="danger">
+          <Button onClick={this.deleteStudent} size="sm" variant="danger">
             Delete
           </Button>
         </td>
